4e198b95e5d2e7ac0641c0982094d4aa95a92095
Ole Kristian Tørresen <tierhon@gmail.com>
2012-04-20 10:56:26 +0200

    removed some unecessary debug stuff

--- extractScaffold.cc
+++ extractScaffold.cc
@@ -245,28 +245,18 @@ void extract_scaffold(string old_bank, string scaffoldid, string new_bank) {
   	vector<Tile_t>::const_iterator ci;
 	//vector<ID_t> & contig_edges = scaffold.getContigEdges();
   	//vector<ID_t>::const_iterator ce;
-  
 
- 	//int chunk = 0;
-  	//string names = "";
 
   	sort(contigs.begin(), contigs.end(), TileOrderCmp());
   	
-    //int bases = 0, span = 0, numcontigs = contigs.size();
-
-	//string seq;
-
-	//int lastrightoffset;
-	//int leftgapsize = 0;
-	//int contignum = 0;
 
 	for (ci = contigs.begin(); ci != contigs.end(); ci++)
 	{
 		Tile_t ctg_tile = *ci;
   		contig_bank.fetch(ci->source, contig);
-    	//cout << "contig.getIID(): " << contig.getIID() << endl;
+    	
 		const std::vector<Tile_t> & tiling = contig.getReadTiling();
-    	//contignum++;
+		
     	vector<Tile_t> new_reads;
     	std::vector<Tile_t>::const_iterator ti;
     	for (ti =  tiling.begin();
@@ -274,44 +264,32 @@ void extract_scaffold(string old_bank, string scaffoldid, string new_bank) {
          ti++)
     	{
     		Tile_t read_tile = *ti;
-    		//Tile_t mate_tile = *ti;
-      		//cout << "read_bank.fetch(ti->source, read), ti->source:" << ti->source << endl;
+
       		read_bank.fetch(ti->source, read);
-      
-      		//cout << "read.getIID(): " << read.getIID() << endl;
-      
+      		
+      		
       		if (!newread_bank.existsEID(read.getEID()))
       		{
-      			//cout << "!newread_bank.existsIID(read.getIID() == True" << endl;
-      			//cout << "read.getFragment(): " << read.getFragment() << endl;
-      			//cout << "read.getFragmentPosition(): " << read.getFragmentPosition() << endl;
+
       			//Got errors because several reads was member of fragment 0, which does not exist. That's because these 'reads' are unitigs.
       			if (read.getFragment() != 0) {
         			fragment_bank.fetch(read.getFragment(), fragment);
-        			//cout << "fragment has IID "<< fragment.getIID() << " and EID " << fragment.getEID() << endl;
         			ID_t read1 = 0, read2 = 0;
         
-        			//cout << "fragment.getIID(): " << fragment.getIID() << endl;
-					// Change: Use old IID for libraries. Done
         			if (!newfragment_bank.existsEID(fragment.getEID()))
         			{
-          				//cout << "!newfragment_bank.existsIID(fragment.getIID() == True" << endl;
           				library_bank.fetch(fragment.getLibrary(), library);
           
-          				//cout << "library.getIID(): " << library.getIID() << endl;
 
           				if (!newlibrary_bank.existsIID(library.getIID()))
           				{
-          					//library.setIID(minSeqID++);
+
           					// Should have the original IIDs
-            					newlibrary_bank.append(library);
+            				newlibrary_bank.append(library);
           				}
 						
 						fragment.setIID(minSeqID++);
 						//Update with the library with new IID						
-						//fragment.setLibrary(library.getIID());
-						
-          				// Need to fix mate pairing below. 
 
           				ID_t mateid = 0;
 
@@ -319,13 +297,11 @@ void extract_scaffold(string old_bank, string scaffoldid, string new_bank) {
           				{
             				mateid = fragment.getMatePair().second;
             				read2 = mateid;
-            				//cout << "Has mate and was read 1 with mateid: " << mateid << endl;
-          				}
+         				}
           				else if (fragment.getMatePair().second == read.getIID())
           				{
             				mateid = fragment.getMatePair().first;
             				read1 = mateid;
-            				//cout << "Has mate and was read 2 with mateid: " << mateid << endl;
           				}
 						
 						// Paired read
@@ -340,35 +316,21 @@ void extract_scaffold(string old_bank, string scaffoldid, string new_bank) {
             				if (mateid == read1) 
             				{
             					fragment.setReads(std::pair<ID_t, ID_t>(mate.getIID(), read.getIID()));
-            					//cout << "Fragment set with read1: " << mate.getIID() << " and read2: " << read.getIID() << endl;
             				}
             				else
             				{
             					fragment.setReads(std::pair<ID_t, ID_t>(read.getIID(), mate.getIID()));
-            					//cout << "Fragment set with read1: " << read.getIID() << " and read2: " << mate.getIID() << endl;
             				}
             				mate.setFragment(fragment.getIID());
             				read.setFragment(fragment.getIID());
-            				
-            				//cout << "Read " << read.getIID() << " has fragment " << read.getFragment() << endl;
-            				//cout << "Mate " << mate.getIID() << " has fragment " << mate.getFragment() << endl;
-            				
-							// Update the read tile
-      						//read_tile.source = mate.getIID();
-      						
-      						Tile_t t_mate;
-      						//t_mate.source = mate.getIID();
-      						
-      						
-      						
+						    
+						    // adds the mate to the new bank, but not added to tile
+						    // that is done below
             				newread_bank.append(mate);
-            				//reads.push_back(mate);
-            				
           				}
           				// Not paired
           				else
           				{
-          					//cout << "Wasn't paired." << endl;
           					read.setIID(minSeqID++);
           					read.setFragment(fragment.getIID());
           				}
@@ -377,7 +339,7 @@ void extract_scaffold(string old_bank, string scaffoldid, string new_bank) {
 					newfragment_bank.append(fragment);
         			newread_bank.append(read);
         			
-      			} //end read.getFragment() != 0
+      			} 
       			else {
       				// is a unitig
       				read.setIID(minSeqID++);
@@ -386,43 +348,36 @@ void extract_scaffold(string old_bank, string scaffoldid, string new_bank) {
       			// Update the read tile
       			read_tile.source = read.getIID();
       			new_reads.push_back(read_tile);
-    		}// end !newread_bank.existsIID(read.getIID())
-    		//
+    		}
     		//If read exists in new bank, and its fragment also exists in new bank, it was probably a mate of an earlier read. 
     		else if (newread_bank.existsEID(read.getEID())) {
+    				// Should not be a unitig, but check for that anyway
     				if (read.getFragment() != 0) {
-    					//cout << "Read already in bank! " << endl;
     					fragment_bank.fetch(read.getFragment(), fragment);
     				
-    					//cout << "fragment has IID "<< fragment.getIID() << " and EID " << fragment.getEID() << endl;
-    					//cout << "newfragment_bank.existsEID(fragment.getEID()) returns " << newfragment_bank.existsEID(fragment.getEID()) << endl;
-    					if (newfragment_bank.existsEID(fragment.getEID())) {
+	   					if (newfragment_bank.existsEID(fragment.getEID())) {
     						Read_t mate;
     						newread_bank.fetch(read.getEID(), mate);
-    					
-    						//cout << "Found read with IID " << mate.getIID() <<" in new bank! " << endl;
+
     						read_tile.source = mate.getIID();
     						new_reads.push_back(read_tile);
-    						//cout << "Added read to tile because both it and its fragment already existed." << endl;
     					}
     				}
     		}
     			
     	}
-    	//cout << "contig.getIID(): " << contig.getIID() << endl;
-
+ 
     	if (!newcontig_bank.existsEID(contig.getEID()))
     	{
     		contig.setIID(minCtgID++);
     		//setReadTiling
     		contig.setReadTiling(new_reads);
-    		//contigs_tile.push_back(contig);
     		newcontig_bank.append(contig);
     	}
     	ctg_tile.source = contig.getIID();
     	new_contigs.push_back(ctg_tile);
     	
-  	} //end for (ti
+  	} 
 	if (!newscaffold_bank.existsEID(scaffold.getEID()))
     {
     	scaffold.setIID(minScfID++);
